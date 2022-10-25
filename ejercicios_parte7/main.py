690cd66eff892ed96b63717f7e595de70f004c44
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-10-25 14:44:29 -0500

    calculadora

---
+++ ejercicios_parte7/main.py
@@ -0,0 +1,15 @@
+from operaciones.calcula import suma, resta, multi, divi
+
+def main():
+   
+    sum = suma(2, 4)
+    print(sum)
+    multipilcacion = multi(2, 3)
+    print(multipilcacion)
+    rest = resta(4, 5)
+    print(rest)
+    division = divi(4, 2)
+    print(division)
+
+if __name__== '__main__':
+    main()
